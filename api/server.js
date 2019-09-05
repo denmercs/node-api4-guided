ad8a55cbe9c89bf01cae6c5e23a475d791d5f125
denmercs <denmercs@gmail.com>
2019-09-05 16:26:23 -0500

    done setting up environment

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const apiRouter = require('./api-router.js');
+const secrets = require("../config/secrets");
+console.log("environment", secrets.environment);
+
+const apiRouter = require("./api-router.js");
 
 const server = express();
 
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 
 module.exports = server;
